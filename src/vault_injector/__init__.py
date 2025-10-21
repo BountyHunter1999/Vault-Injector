46a15b5f3a3077ace5ccee17eb73e70fbc908652
shrish <adhikari.shrish1@gmail.com>
2025-10-21 22:16:28 +0545

    Add main functionality and test for DBDestination in vault_injector

--- src/vault_injector/__init__.py
+++ src/vault_injector/__init__.py
@@ -2,10 +2,28 @@
 Vault Injector package.
 """
 
+from vault_injector.destinations.base import DBDestination
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def main():
     print("Hello from vault_injector")
 
+    destination = DBDestination()
+    with destination:
+        print(destination.client.url)
+        print(destination.client.token)
+        print("seal status", destination.client.seal_status)
+
+        print(destination.client.is_authenticated())
+        # destination.
+
+        destination.client.write(path="test/test", data={"test": "test"})
+        print(destination.ping())
+
+    print(destination.ping())
 
 if __name__ == "__main__":
     main()
