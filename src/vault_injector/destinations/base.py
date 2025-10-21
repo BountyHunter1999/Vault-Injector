0b81254eeedf4fe4cf7ab514aa0c553f9ea8b616
shrish <adhikari.shrish1@gmail.com>
2025-10-21 21:00:08 +0545

    Update .gitignore to include .env file, add pydantic dependency in pyproject.toml, and enhance Destination class with unseal functionality and logging. Introduce tests for vault unsealing process.

--- src/vault_injector/destinations/base.py
+++ src/vault_injector/destinations/base.py
@@ -2,24 +2,93 @@ from abc import ABC, abstractmethod
 from typing import Dict, Any
 from vault_injector.destinations.models import DBData, DBTypeSecret
 import hvac
+import loguru
+import os
 
 
 class Destination(ABC):
-    def __init__(self, vault_client: hvac.Client):
-        self.vault_client = vault_client
+    def __init__(self):
+        self.url = self._get_url()
+        self.token = self._get_token()
+        self.client = hvac.Client(url=self.url, token=self.token)
+
+    @staticmethod
+    def _get_url():
+        return os.getenv("VAULT_ADDR", "http://127.0.0.1:8200")
+    
+    @staticmethod
+    def _get_token():
+        return os.getenv("VAULT_TOKEN", "root")
+    
+    
+    @staticmethod
+    def _get_unseal_keys():
+        return os.getenv("VAULT_UNSEAL_KEYS", "").split(",")
+    
+    def is_authenticated(self) -> bool:
+        """
+        Check if the vault is authenticated.
+        """
+        try:
+            return self.client.is_authenticated()
+        except hvac.exceptions.VaultDown as e:
+            loguru.logger.error(f"Vault is down: {e}", exc_info=True)
+            return False
+
+    def unseal_vault(self):
+        """
+        Unseal the vault if it is sealed.
+        """
+        if not self.is_authenticated():
+            root_token_used = False
+            # current_unseal_key_index = 0
+            unseal_keys = self._get_unseal_keys()
+            while self.client.seal_status.get("sealed", False):
+                if not root_token_used:
+                    self.client.sys.submit_unseal_keys(keys=unseal_keys)
+
+        return True
+                
+    
+    def __enter__(self):
+        
+        # if not self.client.is_authenticated():
+        #     self.client.auth.unseal_vault()
+        if not self.is_authenticated():
+            self.unseal_vault()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if hasattr(self.client, 'logout'):
+            self.client.logout()
+        self.client = None  # Clear the client to make object unusable
 
     @abstractmethod
     def put_data(self, data: Dict[str, Any]):
         raise NotImplementedError
+    
+    def ping(self) -> bool:
+        """
+        Ping the destination to check if it is reachable.
+        """
+        if self.client is None:
+            loguru.logger.error("Vault client has been closed")
+            return False
+            
+        return self.is_authenticated()
 
-class DBDestination(ABC):
+
+    def seal_vault(self):
+        self.client.sys.seal()
+
+class DBDestination(Destination):
     """
     Database Related Secrets, 
     - store database related secrets 
     - get new ephermal secret vault from existing secrets.
     """
-    def __init__(self, vault_client: hvac.Client):
-        super().__init__(vault_client)
+    def __init__(self):
+        super().__init__()
 
     def put_data(self, data: DBData):
         pass
---
+++ tests/test_vault_unseal.py
@@ -0,0 +1,31 @@
+from vault_injector.destinations.base import DBDestination
+import hvac
+import os
+from dotenv import load_dotenv
+import pytest
+import time
+
+
+load_dotenv()
+
+def test_unseal_vault():
+    
+    destination = DBDestination()
+    # print("token", destination._get_token())
+    # print("url", destination._get_url())
+    
+    
+    # Seal the vault if it is not sealed to make test consistent
+    if not destination.client.seal_status.get("sealed", False):
+        destination.seal_vault()
+    
+    # If vault is sealed then we can't get authenticated, so we expect a VaultDown exception
+    assert destination.ping() is False
+    
+    # Unseal the vault and check if we can get authenticated
+    with destination:
+        # destination.unseal_vault()
+        assert destination.ping() is True
+
+    assert destination.ping() is False
+    
